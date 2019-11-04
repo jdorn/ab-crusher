c0acd82dae9d24d0f61a3e130e1cd97cbcfe3b91
Jeremy Dorn <jeremy@jeremydorn.com>
2019-11-04 15:27:31 -0600

    New logo and top nav tweaks.

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,12 @@
 </template>
 
 <style lang="less">
+html,
+body {
+  padding: 0;
+  margin: 0;
+}
+
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -4,24 +4,36 @@
       <img src="/img/icons/android-chrome-192x192.png" />
       AB Crusher
     </div>
-    <router-link to="/tests">Tests</router-link> |
-    <router-link to="/insights">Insights</router-link> |
-    <router-link to="/metrics">Metrics</router-link>
+    <div class="links">
+      <router-link to="/tests">Tests</router-link>
+      <router-link to="/insights">Insights</router-link>
+      <router-link to="/metrics">Metrics</router-link>
+    </div>
   </nav>
 </template>
 
 <style lang="less" scoped>
 nav {
-  padding: 10px;
+  padding: 5px 20px;
   background: #2c3e50;
+  color: #ddd;
+  text-align: left;
+  display: flex;
+  line-height: 40px;
+
+  @media (max-width: 500px) {
+    padding: 5px 10px;
+  }
 
   .logo {
     display: inline-block;
-    vertical-align: middle;
-    color: #42b983;
     font-weight: bold;
     margin-right: 30px;
     font-size: 1.2em;
+
+    @media (max-width: 500px) {
+      margin-right: 15px;
+    }
 
     img {
       width: 40px;
@@ -29,12 +41,23 @@ nav {
     }
   }
 
+  .links {
+    flex: 1;
+  }
+
   a {
-    font-weight: bold;
-    color: #fff;
+    color: #ddd;
+    padding: 0 6px;
+    margin: 0 6px;
+    text-decoration: none;
+
+    @media (max-width: 500px) {
+      margin: 0 3px;
+    }
 
     &.router-link-exact-active {
-      color: #9bb98e;
+      color: #5fd3bc;
+      font-weight: bold;
     }
   }
 }
